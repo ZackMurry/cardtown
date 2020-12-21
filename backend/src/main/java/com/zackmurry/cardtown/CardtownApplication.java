2e2c05d6111b38572ecb1e8457629ba67438a6b0
Zack Murry <zackk.murryy@gmail.com>
2020-12-20 18:54:22 -0600

    Add next js and spring boot auto files

---
+++ backend/src/main/java/com/zackmurry/cardtown/CardtownApplication.java
@@ -0,0 +1,13 @@
+package com.zackmurry.cardtown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardtownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardtownApplication.class, args);
+	}
+
+}
