028def4c61cc3dbbccc359b52e9d9562b8935647
Zack Murry <zackk.murryy@gmail.com>
2021-01-03 20:31:30 -0600

    Connect /cards/import to the backend and improve styling of it

--- frontend/pages/cards/import.js
+++ frontend/pages/cards/import.js
@@ -1,9 +1,15 @@
-import { Typography } from '@material-ui/core'
-import { useEffect, useRef, useState } from 'react'
+import { Button, TextField, Typography } from '@material-ui/core'
+import Cookie from 'js-cookie'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import DashboardSidebar from '../../components/dash/DashboardSidebar'
 import BlackText from '../../components/utils/BlackText'
+import ErrorAlert from '../../components/utils/ErrorAlert'
 import useWindowSize from '../../components/utils/hooks/useWindowSize'
+import SuccessAlert from '../../components/utils/SuccessAlert'
 import theme from '../../components/utils/theme'
+import styles from '../../styles/ImportCards.module.css'
+
+const IMPORT_SUCCESS_TEXT = 'Card successfully imported'
 
 // this simply doesn't support exporting to draft-js, and it'd be hard to make it
 // if the user wants to edit a card, ig a warning will be shown and it will be imported to
@@ -16,6 +22,10 @@ export default function ImportCards() {
   const [ cite, setCite ] = useState('')
   const [ citeInformation, setCiteInformation ] = useState('')
   const [ bodyHtml, setBodyHtml ] = useState('')
+
+  const [ feedbackText, setFeedbackText ] = useState('')
+
+  const jwt = useMemo(() => Cookie.get('jwt'), [ ])
 
   useEffect(() => {
     const processPaste = (elem, pastedData) => {
@@ -57,6 +67,7 @@ export default function ImportCards() {
       return true
     }
 
+    // zero shame: the pasting mechanism is straight from stackoverflow
     pasteInputRef.current.addEventListener('paste', handlePaste)
     return () => pasteInputRef.current?.removeEventListener('paste', handlePaste)
   }, [ ])
@@ -71,19 +82,43 @@ export default function ImportCards() {
     } else if (e.key === 'i') {
       setCiteInformation(window.getSelection().toString())
     } else if (e.key === 'b') {
-      // selection range
       const range = window.getSelection().getRangeAt(0)
-      // plain text of selected range (if you want it w/o html)
-      const text = window.getSelection()
-      // document fragment with html for selection
       const fragment = range.cloneContents()
-      // make new element, insert document fragment, then get innerHTML!
       const div = document.createElement('div')
       div.appendChild(fragment.cloneNode(true))
-      // your document fragment to a string (w/ html)! (yay!)
       const html = div.innerHTML
       setBodyHtml(html)
     }
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    if (tag === '' || cite === '' || bodyHtml === '') {
+      setFeedbackText('Your card must contain a tag, a cite, and a body.')
+      return
+    }
+
+    const response = await fetch('/api/v1/cards', {
+      method: 'POST',
+      headers: { Authorization: `Bearer ${jwt}`, 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        tag,
+        cite,
+        citeInformation,
+        bodyHtml,
+        bodyDraft: 'IMPORTED CARD -- NO DRAFT BODY'
+      }) 
+    })
+    if (response.ok) {
+      setTag('')
+      setCite('')
+      setCiteInformation('')
+      setBodyHtml('')
+      setPData('')
+      setFeedbackText(IMPORT_SUCCESS_TEXT)
+    }
+
   }
 
   return (
@@ -120,7 +155,20 @@ export default function ImportCards() {
             />
           </div>
           <div>
-            <input ref={pasteInputRef} type='text' placeholder='Paste here' />
+            <Typography color='textSecondary' id='tagDescription' style={{ fontSize: 15, margin: '6px 0' }}>
+              First, paste the card into the box below. If you're importing it from a program that supports styled copying and pasting,
+              the card will retain its formatting. With your mouse, select the tag of the card and then press 't' on your keyboard. This will automatically
+              set the selected text to the tag. You can do the same for the rest of the fields using 'c' for cite, 'i' for cite information, and 'b' for body.
+              You'll see their values in the input boxes below your card. You can edit the values of tag, cite, and cite information with the input boxes.
+            </Typography>
+          </div>
+          <div>
+            <input
+              ref={pasteInputRef}
+              type='text'
+              placeholder='Paste here'
+              className={styles['paste-card-input']}
+             />
             <div
               // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
               tabIndex={0}
@@ -129,24 +177,107 @@ export default function ImportCards() {
               onKeyDown={handleKeyDown}
             />
           </div>
-          <div>
-            <Typography>
-              Tag:
-              {` ${tag}`}
-            </Typography>
-            <Typography>
-              Cite:
-              {` ${cite}`}
-            </Typography>
-            <Typography>
-              Cite information:
-              {` ${citeInformation}`}
-            </Typography>
-            <Typography>
-              BodyHtml:
-              {` ${bodyHtml}`}
-            </Typography>
-          </div>
+          <form onSubmit={handleSubmit}>
+            <Button style={{ margin: '1.5vh 0' }} type='submit' variant='contained' color='primary'>
+              Import
+            </Button>
+            {/* tag */}
+            <div>
+              <label htmlFor='tag' id='tagLabel'>
+                <BlackText variant='h3' style={{ fontSize: 18, fontWeight: 500, marginBottom: 5 }}>
+                  Tag
+                  <span style={{ fontWeight: 300 }}>
+                    *
+                  </span>
+                </BlackText>
+              </label>
+              <TextField
+                id='tag'
+                variant='outlined'
+                value={tag}
+                onChange={e => setTag(e.target.value)}
+                style={{ width: '100%', backgroundColor: theme.palette.secondary.main }}
+                multiline
+                rows={2}
+                rowsMax={5}
+                InputProps={{
+                  inputProps: {
+                    name: 'tag',
+                    'aria-labelledby': 'tagLabel',
+                    'aria-describedby': 'tagDescription'
+                  }
+                }}
+              />
+            </div>
+
+            {/* cite */}
+            <div style={{ marginTop: 25 }}>
+              <label htmlFor='cite' id='citeLabel'>
+                <BlackText variant='h3' style={{ fontSize: 18, fontWeight: 500, marginBottom: 5 }}>
+                  Cite
+                  <span style={{ fontWeight: 300 }}>
+                    *
+                  </span>
+                </BlackText>
+              </label>
+              <TextField
+                variant='outlined'
+                value={cite}
+                onChange={e => setCite(e.target.value)}
+                style={{ width: '100%', backgroundColor: theme.palette.secondary.main }}
+                InputProps={{
+                  inputProps: {
+                    name: 'cite',
+                    'aria-labelledby': 'citeLabel',
+                    'aria-describedby': 'citeDescription'
+                  }
+                }}
+              />
+            </div>
+
+            {/* cite information */}
+            <div style={{ marginTop: 25 }}>
+              <label htmlFor='citeInfo' id='citeInfoLabel'>
+                <BlackText variant='h3' style={{ fontSize: 18, fontWeight: 500, marginBottom: 5 }}>
+                  Additional cite information
+                </BlackText>
+              </label>
+              <TextField
+                variant='outlined'
+                value={citeInformation}
+                onChange={e => setCiteInformation(e.target.value)}
+                style={{ width: '100%', backgroundColor: theme.palette.secondary.main }}
+                multiline
+                rowsMax={5}
+                rows={3}
+                InputProps={{
+                  inputProps: {
+                    name: 'citeInfo',
+                    'aria-labelledby': 'citeInfoLabel',
+                    'aria-describedby': 'citeInfoDescription'
+                  }
+                }}
+              />
+            </div>
+
+            {/* body */}
+            <div style={{ marginTop: 25 }}>
+              <BlackText variant='h3' style={{ fontSize: 18, fontWeight: 500 }}>
+                Body
+                <span style={{ fontWeight: 300 }}>
+                  *
+                </span>
+              </BlackText>
+              <div dangerouslySetInnerHTML={{ __html: bodyHtml }} />
+            </div>
+          </form>
+          {
+            feedbackText && (
+              feedbackText === IMPORT_SUCCESS_TEXT
+                ? <SuccessAlert text={feedbackText} onClose={() => setFeedbackText('')} />
+                : <ErrorAlert text={feedbackText} onClose={() => setFeedbackText('')} />
+            )
+          }
         </div>
       </div>
     </div>
